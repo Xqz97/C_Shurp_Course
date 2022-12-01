5086f1955a039da4debc877fc76cf014cfafb446
Roman Turusov <turusov97@yandex.ru>
2022-12-01 16:47:39 +0300

    HomeTask 2_3

---
+++ 2_lesson/Homework/2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+string ThirdNum(int num)
+{
+	if(num > 99)
+    {
+        int numindex = 2; // индекс нужной цифры
+        int num1 = num.ToString()[numindex] - '0';
+        return $"Третья цифра равна: {num1}";
+    }	
+    else
+        return "Третьей цифры нет!";
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(ThirdNum(num));
