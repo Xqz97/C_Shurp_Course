fa0e65a80e3666935a6c2244dcb03c56e5a2501b
Roman Turusov <turusov97@yandex.ru>
2022-12-01 15:50:58 +0300

    Task 2_2

---
+++ 2_lesson/2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// 3. Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+string Crat(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        return("кратно");
+    }
+    else
+        return($"некратно, остаток от деления= {num1 % num2}");
+}
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Crat(num1,num2));
