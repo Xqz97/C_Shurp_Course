0308ada9bbb5346acd1a5fe10f640da6b30bf214
Roman Turusov <turusov97@yandex.ru>
2022-12-07 22:34:47 +0300

    Task 4_1

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// 1. Напишите программу, которая принимаетна вход число и выдаёт количество цифр в числе.
+int InputData(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+int AmouNum(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        int i = 0;
+        while (n > 0)
+        {
+            n = n / 10;
+            i++;
+        }
+        return i;
+    }
+}
+
+int n = InputData("Введите число: ");
+Console.WriteLine(AmouNum(n));
