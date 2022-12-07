1ccbf96d6b71b1a4c5fdb7e5cdf0e9128e42abe0
Roman Turusov <turusov97@yandex.ru>
2022-12-07 22:44:29 +0300

    Task 4_2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Факториал числа N.
+int InputData(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+int Factorial(int n)
+{
+    int fac = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fac *= i;
+    }
+    return fac;
+}
+int n = InputData("Введите число n: ");
+Console.WriteLine(Factorial(n));
+
