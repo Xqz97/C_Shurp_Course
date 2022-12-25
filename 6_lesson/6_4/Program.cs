9c43555939a3c2272245c55b06fe2b3494b4b07f
Roman Turusov <turusov97@yandex.ru>
2022-12-25 19:46:51 +0300

    Task 6_4

---
+++ 6_lesson/6_4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void PrintArrayTd(int[,] array)
+{
+    int raw = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < col; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArrayTd(int raw, int col, int from, int to)
+{
+    int[,] array = new int[raw, col];
+
+    for (int i = 0; i < raw; i++)
+        for (int j = 0; j < col; j++)
+
+            array[i, j] = new Random().Next(from, to);
+    return array;
+}
+
+int[,] CopyArrayTd(int[,] array)
+{
+    int raw = array.GetLength(0);
+    int col = array.GetLength(1);
+    int[,] new_array = new int[raw, col];
+
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < col; j++)
+            new_array[i, j] = array[i, j];
+    }
+    return new_array;
+}
+
+int[,] array_1 = FillArrayTd(int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!));
+PrintArrayTd(array_1);
+Console.WriteLine();
+PrintArrayTd(CopyArrayTd(array_1));
