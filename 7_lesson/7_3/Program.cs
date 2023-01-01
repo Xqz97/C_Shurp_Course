3e947aea6cbb6f8adac6474be2d94f1fa65232b8
Roman Turusov <turusov97@yandex.ru>
2023-01-01 17:47:17 +0300

    Task 7_3

---
+++ 7_lesson/7_3/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задайте двумерный массив. Найдите сумму элементов на главной диагонали.
+*/
+
+void PrintArrayTd(int[,] array)
+{
+    int raw = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < col; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+
+int[,] FillArrayTd(int raw, int col, int from, int to)
+{
+    int[,] array = new int[raw, col];
+
+    for (int i = 0; i < raw; i++)
+        for (int j = 0; j < col; j++)
+
+            array[i, j] = new Random().Next(from, to);
+    return array;
+}
+
+int SumDiagArrayTd(int[,] array)
+{
+    int raw = array.GetLength(0);
+    int col = array.GetLength(1);
+    int sum = 0;
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < col; j++)
+            if (i == j)
+                sum = sum + array[i,j];  
+    }
+    return sum;
+}
+
+int[,] array_1 = FillArrayTd(int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!));
+PrintArrayTd(array_1);
+Console.WriteLine();
+Console.WriteLine(SumDiagArrayTd(array_1));
