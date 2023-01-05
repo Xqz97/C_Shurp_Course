1430d7db6e205e45bb396bef9b87816d093b4f60
Roman Turusov <turusov97@yandex.ru>
2023-01-05 18:12:41 +0300

    Task 8_1

---
+++ 8_lesson/8_1/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задайте двумерный массив. Создайте программу, которая поменяет местами первую и последнюю строку в массиве. */
+
+void PrintArrayTd(int[,] array)
+{
+    int raw = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < col; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+
+int[,] FillArrayTd(int raw, int col, int from, int to)
+{
+    int[,] array = new int[raw, col];
+
+    for (int i = 0; i < raw; i++)
+        for (int j = 0; j < col; j++)
+
+            array[i, j] = new Random().Next(from, to);
+    return array;
+}
+
+void ChangeRawTd(int[,] array)
+{
+    int raw = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    for (int j = 0; j < col; j++)
+    {
+        (array[0, j], array[raw - 1, j]) = (array[raw - 1, j], array[0, j]);
+    }
+}
+
+int[,] array_1 = FillArrayTd(int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!),
+                          int.Parse(Console.ReadLine()!));
+PrintArrayTd(array_1);
+Console.WriteLine();
+ChangeRawTd(array_1);
+PrintArrayTd(array_1);
