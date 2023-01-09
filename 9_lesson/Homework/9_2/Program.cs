d8f1b962a2a1aa79456b5aa5054a410011e02787
Roman Turusov <turusov97@yandex.ru>
2023-01-09 16:46:04 +0300

    homeTask 9_2

---
+++ 9_lesson/Homework/9_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+
+int InputData(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+int Num(int m, int n)
+{
+    if (m > n) return 0;
+    return Num(m + 1, n) + m;
+}
+int m = InputData("Введите M: ");
+int n = InputData("Введите N: ");
+Console.WriteLine(Num(m,n));
+
