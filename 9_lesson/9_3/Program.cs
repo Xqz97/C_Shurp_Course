37e53b7ee2797b486f689be6ccb86d604a2e6c5e
Roman Turusov <turusov97@yandex.ru>
2023-01-09 14:50:35 +0300

    Task 9_3

---
+++ 9_lesson/9_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Написать программу, которая выводит сумму цифр из введенного числа с ипользованием рекурсии.
+*/
+
+
+int InputData(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+int Num(int n)
+{
+
+    if (n == 0) return 0;
+    return Num (n / 10) + n % 10;
+}
+int n = InputData("Введите N: ");
+Console.WriteLine(Num(n));
