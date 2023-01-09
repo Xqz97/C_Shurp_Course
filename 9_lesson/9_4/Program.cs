c9531c621b58cfefa119cdab90c241cfed41e81f
Roman Turusov <turusov97@yandex.ru>
2023-01-09 14:58:31 +0300

    Task 9_4

---
+++ 9_lesson/9_4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Создать программу, в которой пользователь вводит два числа А и В, и число А возводится в степень числа В,
+с помощью рекурсии.
+*/
+
+
+int InputData(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+int Degree(int a, int b)
+{
+    if (b == 0) return 1;
+    return Degree (a, b - 1) * a;
+}
+
+int a = InputData("Введите a: ");
+int b = InputData("Введите b: ");
+Console.WriteLine(Degree(a,b));
